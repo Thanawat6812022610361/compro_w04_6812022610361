1ee3c2d385791d6b6b7cd92a4e2008a6412f0208
Thanwat6812022610361 <noonmobile10947@gmail.com>
2025-07-21 14:54:35 +0700

    none

---
+++ coding_w4_04-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    float score = 90.5;
+    printf("Score: %.1f\n", score);
+    return 0;
+}
+
+// Code ทำงานได้ปกติเพราะ 90.5 เป็นเลขทศนิยม เพราะระบุค่าเป็น float
